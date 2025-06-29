b21e48ca9960022928bcea55c372f49a38f887de
muhammed zaheer mv <136464749+muhdzaheermv@users.noreply.github.com>
2025-06-30 01:10:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ev-olution/"
+  base: '/ev-olution-car-landing-reactjs/', // 👈 matches the repo name
 })
